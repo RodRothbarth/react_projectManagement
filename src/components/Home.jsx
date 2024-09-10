4fd786c86fa6b0fe4afff5a11e9a0e607293190c
rodrothbarth <rodrothbarth@gmail.com>
2024-09-10 09:52:29 -0300

    tailwind organization

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,23 @@
-import img from '../assets/no-projects.png'
+import noProjectImg from "../assets/no-projects.png";
+import { Button } from "./Button.jsx";
 
-export function Home({create}) {
- return (
-  <section>
-      <img src={img} alt="no-project" className='w-16 h-16 object-contain mx-auto'/>
-      <h2>No Project Selected</h2>
-      <p>Select a project or get started with a new one</p>
-      <button onClick={create} className='px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100'>Create new project</button>
-  </section>
- );
+export function Home({ create }) {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img
+        src={noProjectImg}
+        alt="no-project"
+        className="w-16 h-16 object-contain mx-auto"
+      />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with a new one
+      </p>
+      <p className="mt-8">
+        <Button onClick={create}>Create new project</Button>
+      </p>
+    </div>
+  );
 }
--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,13 +1,20 @@
-export function SideMenu({create, projects}) {
- return (
-  <aside className='w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl'>
-      <h2 className='mb-8 font-bold uppercase md:text-xl text-stone-200'>Your Projects</h2>
-   <div  className='flex items-center gap-4'>
-       <button onClick={create} className='px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100'>+ Add Projects</button>
-   </div>
-      <ul className='mt-8'>
-          {projects && projects.map(project=>(<li className='flex justify-between my-4'>{project.name}</li>))}
+import { Button } from "./Button.jsx";
+
+export function SideMenu({ create, projects }) {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        Your Projects
+      </h2>
+      <div className="flex items-center gap-4">
+        <Button onClick={create}>+ Add Project</Button>
+      </div>
+      <ul className="mt-8">
+        {projects &&
+          projects.map((project) => (
+            <li className="flex justify-between my-4">{project.name}</li>
+          ))}
       </ul>
-  </aside>
- );
+    </aside>
+  );
 }
